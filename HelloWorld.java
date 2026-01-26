1123c0f5ac3608853376d2b46757770abef73ca5
Soluchi Fidel-Ibeabuchi <fidelibeabuchi.soluchi@gmail.com>
2026-01-26 11:54:09 -0500

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Name: Soluchi Fidel-Ibeabuchi
+ */
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!!");
+    }
+}
